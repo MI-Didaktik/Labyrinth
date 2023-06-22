ddeb423ef07c5b38efdb049ca8aa814d099e2859
Paul_surface <tryegon@gmail.com>
2023-06-22 17:09:38 +0200

    Fred pfeiltasten bewegung

---
+++ Labyrinth/Fred.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Fred here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Fred extends Actor
+{
+    /**
+     * Act - do whatever the Fred wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("up")) {setRotation(270); move(10);}
+        if(Greenfoot.isKeyDown("down")) {setRotation(90); move(10);}
+        if(Greenfoot.isKeyDown("left")) {setRotation(180); move(10);}
+        if(Greenfoot.isKeyDown("right")) {setRotation(0); move(10);}
+
+    }
+}
